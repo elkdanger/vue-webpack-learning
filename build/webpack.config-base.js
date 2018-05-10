e6f44e459c985b53cc0061598521e9c4bd6a650b
Steve Hobbs <elkdanger@gmail.com>
2018-05-10 11:24:13 +0100

    Upgraded vue-loader to v15

--- build/webpack.config-base.js
+++ build/webpack.config-base.js
@@ -3,22 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const {VueLoaderPlugin} = require('vue-loader')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  mode: isProduction ? 'production' : 'development',
+  mode: isProduction
+    ? 'production'
+    : 'development',
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        sourceMap: true,
-        cache: true
-      }),
+      new UglifyJsPlugin({sourceMap: true, cache: true}),
       new OptimizeCssAssetsPlugin()
     ]
   },
   resolve: {
-    extensions: ['.js', '.json', '.vue'],
+    extensions: [
+      '.js', '.json', '.vue'
+    ],
     alias: {
       '@': path.join(__dirname, '..', 'src'),
       vue: 'vue/dist/vue.js'
@@ -32,28 +34,26 @@ module.exports = {
         options: {
           extract: true
         }
-      },
-      {
+      }, {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
+      }, {
         test: /\.css$/,
         loaders: [
-          isProduction ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+          isProduction
+            ? MiniCssExtractPlugin.loader
+            : 'vue-style-loader',
           'css-loader'
         ]
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    }),
+    new HtmlWebpackPlugin({template: 'index.html'}),
 
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
+    new MiniCssExtractPlugin({filename: '[name].css'}),
+
+    new VueLoaderPlugin()
   ]
 }
